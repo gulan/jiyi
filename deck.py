42ff9f19fd3758f85d36601b6cb1ab724eaa2151
gulan <glen.wilder@gmail.com>
2015-02-07 14:32:01 -0800

    chinese language flashcards via sqlite3

--- deck.py
+++ deck.py
@@ -1,5 +1,11 @@
 
 import random
+import sqlite3
+
+# TBD: Deck is biased to in-memory list.
+# TBD: Extend to manage persistent game state by game-id
+# TBD: select range
+# TBD: sql varient should be a separate package
 
 class Deck(object):
     """ Operations on our card decks"""
@@ -58,3 +64,21 @@ class TestDeck(Deck):
         b = repr(self._save)
         c = repr(self._trash)
         return '\n'.join([a,b,c])
+
+class SQL(Deck):
+        
+    def load(self):
+        # q = """select d.chinese,p.pinyin,d.english 
+        #        from defn d,pinyin p 
+        #        where d.pid = p.pid;"""
+        q = "select chinese,pinyin,english from card;"
+        self._save = [((c,p),e)  for (c,p,e) in self.cx.execute(q)]
+        self.redo()
+
+    def __init__(self,db_path):
+        self.db_path = db_path
+        self.cx = sqlite3.connect(db_path)
+        self._deck = []
+        self._save = []
+        self._trash = []
+
--- jiyi_web.py
+++ jiyi_web.py
@@ -7,30 +7,8 @@ import multiprocessing as mp
 import os
 import sys
 
-question_html = '''<!DOCTYPE html>
-<html>
-  <head><title>SnapFlash</title></head>
-  <body>
-    <form action="/jiyi" method="post">
-      <input name="restack" value="Review" type="submit">
-      <input name="show" value="Show" type="submit">
-    </form>
-    <p>{{ question }}</p>
-  </body>
-</html>'''
+# TBD: select game varient by URL
 
-answer_html = '''<!DOCTYPE html>
-<html>
-  <head><title>SnapFlash</title></head>
-  <body>
-    <form action="/jiyi" method="post">
-      <input name="discard" value="Got It!" type="submit">
-      <input name="retry" value="Try Again" type="submit">
-    </form>
-    <p>{{ question }}</p>
-    <p>{{ answer }}</p>
-  </body>
-</html>'''
 
 class web_adaptor(object):
     """The backend finite machine model should not know that the
@@ -39,33 +17,94 @@ class web_adaptor(object):
     about how our game is played. But the two components need to talk
     to each other, so we need a part to translate and exchange
     messages. That is the job of this adaptor."""
-    
+
+    question_html = '''<!DOCTYPE html>
+    <html>
+      <head><title>SnapFlash</title></head>
+      <body>
+        <form action="/jiyi" method="post">
+          <input name="restack" value="Review" type="submit">
+          <input name="show" value="Show" type="submit">
+        </form>
+        <p>{{ question }}</p>
+      </body>
+    </html>'''
+
+    answer_html = '''<!DOCTYPE html>
+    <html>
+      <head><title>SnapFlash</title></head>
+      <body>
+        <form action="/jiyi" method="post">
+          <input name="discard" value="Got It!" type="submit">
+          <input name="retry" value="Try Again" type="submit">
+        </form>
+        <p>{{ question }}</p>
+        <p>{{ answer }}</p>
+      </body>
+    </html>'''
+      
     def send_event(self,msg):
         def make_event():
             # a click has the same resentation as a machine event.
             return msg
         self.event_queue.put(make_event())
         
-    def receive_event(self):
-        return self.event_queue.get()
+    def receive_event(self): return self.event_queue.get()
+    def receive_page(self): return self.response_queue.get()
     
     def send_question(self,question):
-        html = template(question_html,
+        html = template(self.question_html,
                         question=question)
         self.response_queue.put(html)
         
     def send_answer(self,question,answer):
-        html = template(answer_html,
+        html = template(self.answer_html,
                         question=question,
                         answer=answer)
         self.response_queue.put(html)
         
-    def receive_page(self):
-        return self.response_queue.get()
-    
     def __init__(self):
         self.event_queue = mp.Queue(1)
         self.response_queue = mp.Queue(1)
+
+class sql_adaptor(web_adaptor):
+    question_html = '''<!DOCTYPE html>
+    <html>
+      <head><title>SnapFlash</title></head>
+      <body>
+        <form action="/jiyi" method="post">
+          <input name="restack" value="Review" type="submit">
+          <input name="show" value="Show" type="submit">
+        </form>
+        <p>{{ chinese }}<br>{{ pinyin }}</p>
+      </body>
+    </html>'''
+
+    answer_html = '''<!DOCTYPE html>
+    <html>
+      <head><title>SnapFlash</title></head>
+      <body>
+        <form action="/jiyi" method="post">
+          <input name="discard" value="Got It!" type="submit">
+          <input name="retry" value="Try Again" type="submit">
+        </form>
+        <p>{{ chinese }}<br>{{ pinyin }}</p>
+        <p>{{ english }}</p>
+      </body>
+    </html>'''
+
+    def send_question(self,question):
+        html = template(self.question_html,
+                        chinese=question[0],
+                        pinyin=question[1])
+        self.response_queue.put(html)
+        
+    def send_answer(self,question,answer):
+        html = template(self.answer_html,
+                        chinese=question[0],
+                        pinyin=question[1],
+                        english=answer)
+        self.response_queue.put(html)
 
 @get('/jiyi')
 def start():
@@ -78,9 +117,18 @@ def play():
     adaptor.send_event(click)
     return adaptor.receive_page()
 
+def test_deck():
+    return (web_adaptor(),deck.TestDeck)
+
+def sql_deck():
+    adaptor = sql_adaptor()
+    mk = lambda : deck.SQL('hsk1.db')
+    return (adaptor,mk)
+
 if __name__ == '__main__':
-    adaptor = web_adaptor()
-    sm = mp.Process(target=machine.machine,args=(adaptor,deck.TestDeck))
+    args = sql_deck()
+    adaptor = args[0] # global :(
+    sm = mp.Process(target=machine.machine,args=args)
     sm.start()
     run(host='0.0.0.0',port=8080,debug=True)
     sm.join()
