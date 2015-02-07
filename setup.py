930a304ae95a7ba8dea4b0a386c949132588b4ea
gulan <glen.wilder@gmail.com>
2015-02-07 14:43:09 -0800

    with sqlite

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@
 
 from distutils.core import setup
 
+# manually: sqlite hsk1.db <hsk1.sql  
+
 setup(name='jiyi',
-      version='0.0.0',
+      version='0.1.0',
       description='Flash Card Game',
       author='gulan',
       author_email='glen.wilder@gmail.com',
